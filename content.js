cacb2a5aa770d3917fa86f6d2afd23bba1ef5c31
insaan21 <ishaan.guha@gmail.com>
2020-06-08 04:42:37 -0500

    basic apimodel using nodejs

--- content.js
+++
@@ -1,9 +0,0 @@
-var userComment = ""
-chrome.storage.sync.get('myLine', function(data){
-    userComment = data.myLine;
-})
-console.log(userComment);
-        chrome.runtime.sendMessage({
-            url: window.location.href,
-            comment: userComment
-        })
--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 window.onload = function() {
     document.getElementById('save').onclick = function(){
-        var value = "";
-        chrome.storage.sync.get('myLine', function(data){
-           value = data.myLine; 
-        })
-        value += document.getElementById('saveLine').value
-        chrome.storage.sync.set({'myLine': value})
-        const bg = chrome.extension.getBackgroundPage()
-        const div = document.createElement('div')
-        div.textContent = bg.comments[window.location.href]
-        document.body.appendChild(div)
+                var comment = document.getElementById('comment').value
+                var name = document.getElementById('name').value
+                var request1 = new XMLHttpRequest();
+                var url = 'http://localhost:3000/add/' + comment + '/' + name 
+                request1.open('GET', url)
+                request1.send()
+               fetch("./comments.json")
+                    .then(function(resp){
+                        return resp.json();
+                    })
+                    .then(function(data){
+                        var comments = Object.keys(data)
+                        console.log(comments);
+                        for(var i = 0; i < comments.length; i++){
+                            var currentName = data[comments[i]]
+                            const div = document.createElement('div')
+                            div.textContent = currentName + ": " + comments[i]
+                            document.body.appendChild(div)
+                        }
+                    })
+        }
+       
     }
-}
